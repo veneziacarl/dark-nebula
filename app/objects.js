c48031485a4cd83ebd7f45e607d754a9b087b456
Carl Venezia <veneziacarl@gmail.com>
2015-11-30 18:23:34 -0500

    finished 2 of 5 optional challenges

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-    
+    obj['ourFn'] = fn
+    return obj.ourFn();
   },
 
   alterObjects : function(constructor, greeting) {
-    // Optional Challenge
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-    // Optional Challenge
+    dataArray = [];
+    for (var name in obj) {
+      if (obj.hasOwnProperty(name)){
+        dataArray.push(name + ": " + obj[name]);
+      };
+    };
+    return dataArray;
   }
 };
--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,35 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-    // Optional Challenge
+    var fileList = [];
+    var foundDirectory = null;
+    var findDirectory = function(dataObject, targetDirName) {
+      if (dataObject.dir === targetDirName) {
+        foundDirectory = dataObject;
+      } else {
+        for (var i = 0; i < dataObject.files.length; i++) {
+          if (typeof dataObject.files[i] === 'object') {
+            findDirectory(dataObject.files[i], targetDirName);
+          };
+        };
+      };
+    };
+    var recursiveFunction = function(fileFolder) {
+      for (var i = 0; i < fileFolder.files.length; i++) {
+        if (typeof fileFolder.files[i] === 'object') {
+          recursiveFunction(fileFolder.files[i]);
+        } else {
+            fileList.push(fileFolder.files[i]);
+        };
+      };
+    };
+    if (dirName === undefined) {
+      recursiveFunction(data);
+    } else {
+      findDirectory(data, dirName);
+      recursiveFunction(foundDirectory);
+    };
+    return fileList;
   },
 
   permute: function(arr) {
@@ -17,3 +45,11 @@ exports.recursionAnswers = {
     // Optional Challenge
   }
 };
+
+// for (var i = 0; i < fileFolder.length; i++) {
+//   if (typeof fileFolder[i] === 'object') {
+//     recursiveFunction(fileFolder[i].files);
+//   } else {
+//     fileList.push(fileFolder[i]);
+//   };
+// };
